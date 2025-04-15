f07f8d77a0f6109d2005f1ae91fcb96a1b28cf9c
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-15 11:27:52 +0900

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -39,4 +39,13 @@ bool	is_same_str(const char *s1, const char *s2);
  */
 bool	is_special_char(char c);
 
+/**
+ * @brief 空白に相当する文字かどうか判別する関数
+ * 
+ * @return true | false
+ * 
+ * @param c 判別したい文字
+ */
+bool	is_space(char c);
+
 #endif
