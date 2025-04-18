13d0acca7f57a95fcaf57f9061009992285f9e83
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-18 10:50:54 +0900

    Update token.c

--- src/debugger/token.c
+++ src/debugger/token.c
@@ -1,4 +1,4 @@
-#include "../../include/debug.h"
+#include "../../include/debugger.h"
 
 static void	print_token_type(t_token_type token_type)
 {
@@ -14,19 +14,45 @@ static void	print_token_type(t_token_type token_type)
 	ft_putstr_fd(RESET, STDOUT_FILENO);
 }
 
-void	print_token(t_token token)
+void	print_token(t_token *token)
 {
+	if (!token)
+	{
+		ft_putstr_fd(BOLD"null\n"RESET, STDOUT_FILENO);
+		return ;
+	}
 	ft_putstr_fd("{ type: ", STDOUT_FILENO);
-	print_token_type(token.type);
+	print_token_type(token->type);
 	ft_putstr_fd(", value: "GREEN"'", STDOUT_FILENO);
-	ft_putstr_fd(token.value, STDOUT_FILENO);
+	ft_putstr_fd(token->value, STDOUT_FILENO);
 	ft_putstr_fd("'"RESET, STDOUT_FILENO);
 	ft_putstr_fd(" }", STDOUT_FILENO);
+}
+
+void	print_tokens(t_token **tokens)
+{
+	int	i;
+
+	if (!tokens)
+	{
+		ft_putstr_fd(BOLD"null\n"RESET, STDOUT_FILENO);
+		return ;
+	}
+	i = 0;
+	ft_putstr_fd("[\n", STDOUT_FILENO);
+	while (tokens[i])
+	{
+		ft_putstr_fd("  ", STDOUT_FILENO);
+		print_token(tokens[i]);
+		ft_putstr_fd(",\n", STDOUT_FILENO);
+		i++;
+	}
+	ft_putstr_fd("  "BOLD"null"RESET"\n]\n", STDOUT_FILENO);
 }
 
 // int main() {
 // 	t_token	token;
 // 	token.value = "test";
 // 	token.type = REDIRECT;
-// 	print_token(token);
+// 	print_token(&token);
 // }
