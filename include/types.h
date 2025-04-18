baf2c1ec44d5f95833a54775acbb08c94f7851a6
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-18 14:30:40 +0900

    define pipeline type

--- include/types.h
+++ include/types.h
@@ -64,4 +64,27 @@ typedef struct s_token
 	t_str_heap		value;
 }	t_token;
 
+typedef struct s_command
+{
+	t_str_arr_heap	args;
+	t_str_heap		input_file;
+	t_str_heap		output_file;
+	bool			append_output;
+	t_str_heap		heredoc;
+}	t_command;
+
+typedef enum e_chain_operator
+{
+	CHAIN_AND,
+	CHAIN_OR,
+}	t_chain_operator;
+
+typedef struct s_pipeline
+{
+	t_command			**commands;
+	int					command_count;
+	t_chain_operator	operator;
+	t_pipeline			*next;
+}	t_pipeline;
+
 #endif
