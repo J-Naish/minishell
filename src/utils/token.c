51355565941a1a93510f06113a92678c89fcc9eb
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-20 18:31:50 +0900

    create a func to determine if a token is word

--- src/utils/token.c
+++ src/utils/token.c
@@ -2,10 +2,24 @@
 
 bool	is_pipe(t_token *token)
 {
-	return (token->type == TOKEN_REDIRECT && is_same_str(token->value, "|"));
+	if (token->type == TOKEN_REDIRECT && is_same_str(token->value, "|"))
+		return (true);
+	else
+		return (false);
 }
 
 bool	is_chain(t_token *token)
 {
-	return (token->type == TOKEN_CHAIN);
+	if (token->type == TOKEN_CHAIN)
+		return (true);
+	else
+		return (false);
+}
+
+bool	is_word(t_token *token)
+{
+	if (token->type == TOKEN_WORD)
+		return (true);
+	else
+		return (false);
 }
