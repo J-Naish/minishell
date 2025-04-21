73fe5c28636271235c9e5b7041dac748e64f513c
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-21 11:00:29 +0900

    implement echo command

--- include/builtins.h
+++ include/builtins.h
@@ -3,6 +3,7 @@
 
 # include "./minishell.h"
 
+void	cmd_echo(t_command *command);
 void	cmd_exit(t_command *command);
 
 #endif
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,47 +1,27 @@
 #include "../../include/builtins.h"
 
-// クォートまで出力されてる
-// void	cmd_echo(t_command *command)
-// {
-// 	int		i;
-// 	bool	is_n_option;
+void	cmd_echo(t_command *command)
+{
+	int		i;
+	bool	is_n_option;
 
-// 	if (is_same_str(command->args[1], "-n"))
-// 	{
-// 		is_n_option = true;
-// 		i = 2;
-// 	}
-// 	else
-// 	{
-// 		is_n_option = false;
-// 		i = 1;
-// 	}
-// 	while (command->args[i])
-// 	{
-// 		printf("%s", command->args[i]);
-// 		i++;
-// 		if (command->args[i])
-// 			printf(" ");
-// 	}
-// 	if (!is_n_option)
-// 		printf("\n");
-// }
-
-// int main() {
-// 	char *test1[] = {
-// 		"echo"
-// 		"lorem",
-// 		"ipsum",
-// 		"dollar",
-// 		"sit",
-// 		"ammet",
-// 		NULL
-// 	};
-// 	cmd_echo(test1);
-// 	cmd_echo(test1);
-// 	char *test2[] = {
-// 		NULL
-// 	};
-// 	cmd_echo(test2);
-// 	cmd_echo(test2);
-// }
+	if (command->args[1] && is_same_str(command->args[1], "-n"))
+	{
+		is_n_option = true;
+		i = 2;
+	}
+	else
+	{
+		is_n_option = false;
+		i = 1;
+	}
+	while (command->args[i])
+	{
+		ft_putstr_fd(command->args[i], STDOUT_FILENO);
+		i++;
+		if (command->args[i])
+			ft_putstr_fd(" ", STDOUT_FILENO);
+	}
+	if (!is_n_option)
+		ft_putstr_fd("\n", STDOUT_FILENO);
+}
