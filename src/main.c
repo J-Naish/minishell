ddbfd9ab2768a79702f61c7ac8ff1b927c376e70
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-21 11:12:47 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,6 +7,13 @@ static void	init_signal(void)
 	g_signal.status = 0;
 }
 
+static void	free_all(t_pipeline **pls, t_token **tkns, t_str_heap prompt)
+{
+	free_pipelines(pls);
+	free_tokens(tkns);
+	free(prompt);
+}
+
 int	main(void)
 {
 	t_str_heap	prompt;
@@ -26,11 +33,17 @@ int	main(void)
 		}
 		add_history(prompt);
 		tokens = parse(prompt);
+		if (!validate_tokens(tokens))
+		{
+			free_tokens(tokens);
+			free(prompt);
+			rl_on_new_line();
+			continue ;
+		}
 		pipelines = build_pipeline(tokens);
 		print_pipelines(pipelines);
 		rl_on_new_line();
-		free_pipelines(pipelines);
-		free_tokens(tokens);
+		free_all(pipelines, tokens, prompt);
 	}
 	return (EXIT_SUCCESS);
 }
