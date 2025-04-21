5a9d23cbb0fc9505bd42a3e2f62e8b570f2cab64
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-22 04:40:07 +0900

    Update pwd.c

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -2,5 +2,21 @@
 
 void	cmd_pwd(t_command *command)
 {
-	(void)command;
+	char	*cwd;
+	char	buffer[PATH_MAX];
+
+	if (command->args[1] && starts_with(command->args[1], "-")
+		&& ft_strlen(command->args[1]) > 1)
+	{
+		put_error(SHELL_NAME": no option is available\n");
+		exit(EXIT_FAILURE);
+	}
+	cwd = getcwd(buffer, sizeof(buffer));
+	if (!cwd)
+	{
+		perror(SHELL_NAME": ");
+		exit(EXIT_FAILURE);
+	}
+	ft_putstr_fd(cwd, STDOUT_FILENO);
+	ft_putstr_fd("\n", STDOUT_FILENO);
 }
