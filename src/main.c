c86f2ad244615e7b37b29bae3d7c771311a33435
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-21 11:08:02 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,14 +10,27 @@ static void	init_signal(void)
 int	main(void)
 {
 	t_str_heap	prompt;
+	t_token		**tokens;
+	t_pipeline	**pipelines;
 
 	init_signal();
 	while (1)
 	{
 		prompt = readline(SHELL_NAME"$ ");
-		if (prompt && *prompt)
-			add_history(prompt);
+		if (!prompt)
+			continue ;
+		if (!*prompt)
+		{
+			free(prompt);
+			continue ;
+		}
+		add_history(prompt);
+		tokens = parse(prompt);
+		pipelines = build_pipeline(tokens);
+		print_pipelines(pipelines);
 		rl_on_new_line();
+		free_pipelines(pipelines);
+		free_tokens(tokens);
 	}
 	return (EXIT_SUCCESS);
 }
