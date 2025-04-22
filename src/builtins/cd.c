a03538ab42519a9e7b3f3a87ed7ec162623e7710
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-22 19:08:05 +0900

    Update cd.c

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -2,16 +2,24 @@
 
 void	cmd_cd(t_command *command)
 {
-	t_str	target_dir;
+	t_str_heap	target_dir;
+	t_str		env_home;
 
-	if (!command->args[1] || is_same_str(command->args[1], "~"))
+	if (command->args[1] && starts_with(command->args[1], "~"))
 	{
-		target_dir = getenv("HOME");
-		if (!target_dir)
+		env_home = getenv("HOME");
+		if (!env_home)
 			put_error(SHELL_NAME": path is unset\n");
+		if (ft_strlen(command->args[1]) > 1)
+			target_dir = ft_strjoin(env_home, &command->args[1][1]);
+		else
+			target_dir = ft_strdup(env_home);
 	}
+	else if (!command->args[1])
+		target_dir = ft_strdup(getenv("HOME"));
 	else
-		target_dir = command->args[1];
+		target_dir = ft_strdup(command->args[1]);
 	if (chdir(target_dir) == -1)
 		perror(SHELL_NAME": cd");
+	free(target_dir);
 }
