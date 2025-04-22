5692adf535caa07266cb8ad141d75f7659481c7b
J-Naish <mjyrgd2332@i.softbank.jp>
2025-04-22 10:32:18 +0900

    implement cd command

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -2,5 +2,16 @@
 
 void	cmd_cd(t_command *command)
 {
-	(void)command;
+	t_str	target_dir;
+
+	if (!command->args[1] || is_same_str(command->args[1], "~"))
+	{
+		target_dir = getenv("HOME");
+		if (!target_dir)
+			put_error(SHELL_NAME": path is unset\n");
+	}
+	else
+		target_dir = command->args[1];
+	if (chdir(target_dir) == -1)
+		perror(SHELL_NAME": cd");
 }
