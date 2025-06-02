d54ebf33ee959a1d2c48ad0b7fee8497bc31dc9b
J-Naish <mjyrgd2332@i.softbank.jp>
2025-06-02 22:47:36 +0900

    Update export.c

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -5,8 +5,35 @@
 
 // }
 
+// static void	replace_env(char *new_env, char ***envpp)
+// {
+
+// }
+
+static void	add_env(char *new_env, char ***envpp)
+{
+	int				size;
+	int				i;
+	t_str_arr_heap	new;
+
+	size = 0;
+	while ((*envpp)[size])
+		size++;
+	new = (t_str_arr_heap)malloc(sizeof(t_str *) * (size + 2));
+	i = 0;
+	while (i < size - 1)
+	{
+		new[i] = ft_strdup((*envpp)[i]);
+		i++;
+	}
+	new[i] = ft_strdup(new_env);
+	new[i + 1] = ft_strdup((*envpp)[i]);
+	new[i + 2] = NULL;
+	free_str_arr(*envpp);
+	*envpp = new;
+}
+
 void	cmd_export(t_command *command, char ***envpp)
 {
-	(void)command;
-	(void)envpp;
+	add_env(command->args[1], envpp);
 }
